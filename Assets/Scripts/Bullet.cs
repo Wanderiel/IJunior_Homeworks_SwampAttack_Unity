71c095ddace7d1b561e82adc5802ae6219e54a46
Pavel Dymov <dpavel82@yandex.ru>
2023-07-08 18:18:57 +0300

    Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] protected int Damage;
+    [SerializeField] protected float Speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * Speed * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.TryGetComponent(out Enemy enemy))
+        {
+            enemy.TakeDamage(Damage);
+
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.TryGetComponent(out Wall wall))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
